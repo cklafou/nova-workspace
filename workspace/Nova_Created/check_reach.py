4e0214c429f2a87cd78f1f8b7641406e5985d598
cklafou <Lafountainer@gmail.com>
2026-07-23 08:19:59 +0900

    auto-commit 2026-07-23 08:19:59

---
+++ workspace/Nova_Created/check_reach.py
@@ -0,0 +1,10 @@
+# Last updated: 2026-07-23 08:16:37
+import json
+data = json.load(open('../Tasking/tasks.json'))
+print(type(data))
+if isinstance(data, dict):
+    tasks = data.get('tasks', data)
+else:
+    tasks = data
+for t in tasks[:3]:
+    print(type(t), list(t) if isinstance(t,dict) else t[:80])
